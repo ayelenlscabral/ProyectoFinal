90e560ded9aaa01c36ecfb4dae5bf35ab109acc5
sastremax <141637332+sastremax@users.noreply.github.com>
2023-10-03 14:04:46 -0300

    se agrega 1 cliente en el main
    falta completar

--- Veterinaria/src/veterinaria/Veterinaria.java
+++ Veterinaria/src/veterinaria/Veterinaria.java
@@ -1,9 +1,20 @@
 package veterinaria;
 
+import veterinaria.AccesoADatos.ClienteData;
+import veterinaria.Entidades.Cliente;
+
 public class Veterinaria {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        ClienteData clienteData = new ClienteData();
+        
+        Cliente cliente1 = new Cliente(25145269, "Sastre", "Maximiliano", "Arcoiris 2072", 1166529746, "Perez Bruno Mariana", true);
+        
+        clienteData.guardarCliente(cliente1);
+        
+        cliente1.setTelefono(1131899378);
+        clienteData.modificarCliente(cliente1);
+        
     }
     
 }
