48fe086c2f1d45b1b30b1e0fce51be69455888d8
sastremax <141637332+sastremax@users.noreply.github.com>
2023-10-03 10:27:44 -0300

    se completa la clase cliente
    clase cliente completa

--- Veterinaria/src/veterinaria/Entidades/Cliente.java
+++ Veterinaria/src/veterinaria/Entidades/Cliente.java
@@ -1,5 +1,107 @@
 package veterinaria.Entidades;
 
 public class Cliente {
-    
+
+    public int idCliente;
+    public int dni;
+    public String apellido;
+    public String nombre;
+    public String direccion;
+    public int telefono;
+    public String personaAlternativa;
+    public boolean estado;
+
+    public Cliente() {
+    }
+
+    public Cliente(int dni, String apellido, String nombre, String direccion, int telefono, String personaAlternativa, boolean estado) {
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.personaAlternativa = personaAlternativa;
+        this.estado = estado;
+    }
+
+    public Cliente(int idCliente, int dni, String apellido, String nombre, String direccion, int telefono, String personaAlternativa, boolean estado) {
+        this.idCliente = idCliente;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.personaAlternativa = personaAlternativa;
+        this.estado = estado;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getPersonaAlternativa() {
+        return personaAlternativa;
+    }
+
+    public void setPersonaAlternativa(String personaAlternativa) {
+        this.personaAlternativa = personaAlternativa;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return idCliente + dni + apellido + nombre + direccion + telefono + personaAlternativa + estado;
+    }
+
 }
